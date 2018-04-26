e06ceb669f94e307a5bd544735f72af7f36a5967
silverstar <silverstart1987913@yahoo.com>
2018-04-26 08:59:22 -0700

    add win check

---
+++ process_sandiego_win.py
@@ -0,0 +1,31 @@
+import os
+import subprocess
+import re
+import argparse
+import psutil
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Do something.")
+    parser.add_argument('-i', '--index', type=int, required=True, help='Index of letter from 10 ~ 36')
+
+    args = parser.parse_args()
+    method = args.method
+    processname = 'python random_sandiego.py -i {}'.format(index)
+
+    process_running = False 
+    for process in psutil.process_iter():
+        try:
+            cmdline = process.cmdline()
+            for t in cmdline:
+                if "bot" in t:
+                    print cmdline
+
+                    if cmdline[3] == str(method):
+                        process_running = True
+        except:
+            pass
+
+    if process_running == True:
+        print(' processes running of ', processname, 'type')
+    else:
+        os.system(processname)
